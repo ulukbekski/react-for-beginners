66a96f1e09f76a80d4d32ccc7083342d59d67fa5
ulukbekski <88556249+ulukbekski@users.noreply.github.com>
2022-11-17 16:08:19 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import {React, useState , useEffect} from "react";
-import './index.scss'
+import {React} from "react";
 import { ImArrowRight2 } from 'react-icons/im'
-
 
 const fruits = ['яблоко','банан','ананас','кокос','вишня','персик','лимон']
 
 function App() {
-
   return(
     <div className="App">
-      <h1>Создай логику программы которая будет перебирать следующий элемент из списка</h1>
-
       <ul >
         <li>{fruits[0]}</li>
       </ul>
-
      <button >next <ImArrowRight2 className="icon"/> </button> 
-
     </div>
   )
 }
