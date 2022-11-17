05facf77f04727b7406cdbeaa16dcc1c7deb795b
ulukbekski <88556249+ulukbekski@users.noreply.github.com>
2022-11-17 12:41:12 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState , useEffect} from "react";
+import './index.scss'
+import { ImArrowRight2 } from 'react-icons/im'
+
+
+const fruits = ['яблоко','банан','ананас','кокос','вишня','персик','лимон']
 
 function App() {
-  return (
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Создай логику программы которая будет перебирать следующий элемент из списка</h1>
+
+      <ul >
+        <li>{fruits[0]}</li>
+      </ul>
+
+     <button >next <ImArrowRight2 className="icon"/> </button> 
+
     </div>
-  );
+  )
 }
 
 export default App;
